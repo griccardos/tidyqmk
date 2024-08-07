d7d2491dfd3953a99fcb654693611ca4ab4c430d
Riccardo <riccardo@rair>
2024-08-07 09:11:09 +0200

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use easier::prelude::*;
 use pest::{
     error::{Error, ErrorVariant},
     Parser,
@@ -8,9 +9,9 @@ fn main() {
     let example = r#"
         [0] = LAYOUT_universal(
           KC_Q, KC_W, KC_F, KC_P, KC_B,                                             KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN,
-          LSFT_T(KC_A), LCTL_T(KC_R), LALT_T(KC_S), LGUI_T(KC_T), KC_G,             KC_M, RGUI_T(KC_N), RALT_T(KC_E), RCTL_T(KC_I), RSFT_T( KC_O),
+          LSFT_T(KC_A), KC_R, LALT_T(KC_S), LGUI_T(KC_T), KC_G,             KC_M, RGUI_T(KC_N), RALT_T(KC_E), KC_I, RSFT_T( KC_O),
           KC_Z, KC_X, KC_C, KC_D, KC_V,                                             KC_K, KC_H, KC_COMM, KC_DOT, KC_QUOTE,
-          CW_TOGG , QK_REP , KC_DEL, LT(2,KC_TAB) , LT(1,KC_SPACE), LT(3,KC_ESC),   LSFT_T(KC_ENT) , LT(2,KC_BSPC),KC_NO,KC_NO,KC_NO,   SCRL_TO
+          CW_TOGG , QK_REP , KC_DEL, LT(2,KC_TAB) , LT(1,KC_SPC), LT(3,KC_ESC),   KC_ENT , LT(2,KC_BSPC),KC_NO,KC_NO,KC_NO,   SCRL_TO
         )"#;
 
     let mut prog = match MyParser::parse(Rule::programouter, example) {
@@ -73,9 +74,8 @@ fn format(pair: pest::iterators::Pair<Rule>) -> String {
         }
 
         Rule::layer => {
-            let mut line_codes = vec![];
-
             let lines = pair.into_inner();
+            let mut line_codes = vec![];
             for line in lines {
                 let mut keycodes = vec![];
                 for keycode in line.into_inner() {
@@ -83,43 +83,74 @@ fn format(pair: pest::iterators::Pair<Rule>) -> String {
                 }
                 line_codes.push(keycodes);
             }
+
             let max_cols = line_codes.iter().map(|x| x.len()).max().unwrap();
-            let max_len = line_codes.iter().fold(
-                std::iter::repeat(0).take(max_cols).collect(),
-                |acc: Vec<usize>, line| {
-                    acc.iter()
-                        .zip(line.iter().chain(std::iter::repeat(&"".to_string()))) //need to pad shorter cols
-                        .map(|(a, l)| l.len().max(*a))
-                        .collect()
-                },
-            );
-
-            for line in &line_codes {
-                for (i, code) in line.iter().enumerate() {
-                    let width = max_len[i] + 1;
-                    let centre = line.len() / 2;
-
-                    if i >= centre {
-                        result.push_str(&format!("{: <1$},", code, width));
-                    } else {
-                        result.push_str(&format!("{: >1$},", code, width));
-                    }
-
-                    if i == centre - 1 {
-                        result.push_str("_____");
-                    }
+            //for split, we want to arrange the columns in a way that the middle column is in the middle
+            //and the columns are split around it with free space on the outside
+            let mut grid: Vec<Vec<Option<String>>> =
+                line_codes.iter().map(|_| vec![None; max_cols]).to_vec();
+            for (gr, line) in grid.iter_mut().zip(line_codes.into_iter()) {
+                let start = (max_cols - line.len()) / 2;
+                for i in 0..line.len() {
+                    gr[start + i] = Some(line[i].clone());
                 }
-                result.push_str("\n");
             }
+            result.push_str(print_grid(grid).as_str());
         }
         Rule::layernum => {
             result.push_str(pair.as_str());
         }
-        Rule::line => {} //not used, because we operate on the codes in it
-
+        Rule::validname => {
+            result.push_str(pair.as_str());
+        }
+        //not used, because we operate on the codes in it
+        Rule::line => {}
+        //these are all ignored
         Rule::WHITESPACE => {}
         Rule::EOI => {}
         Rule::programouter => {}
+        Rule::number => {}
+    }
+    result
+}
+fn print_grid(grid: Vec<Vec<Option<String>>>) -> String {
+    let mut result = String::new();
+    let max_cols = grid[0].len();
+    let max_len = grid.iter().fold(
+        std::iter::repeat(0).take(max_cols).collect(),
+        |acc: Vec<usize>, line| {
+            acc.iter()
+                .zip(line.iter())
+                .map(|(a, l)| l.as_ref().map(|a| a.len()).unwrap_or_default().max(*a))
+                .collect()
+        },
+    );
+
+    for (li, line) in grid.iter().enumerate() {
+        for (i, code) in line.iter().enumerate() {
+            let width = max_len[i] + 1;
+            let centre = line.len() / 2;
+            let comma = if li == grid.len() - 1 && i == line.len() - 1 {
+                ""
+            } else {
+                ","
+            };
+            match code {
+                Some(code) => {
+                    if i >= centre {
+                        result.push_str(&format!("{: <1$}{comma}", code, width));
+                    } else {
+                        result.push_str(&format!("{: >1$}{comma}", code, width));
+                    }
+                }
+                None => result.push_str(&format!("{: >1$}", " ", width + 1)), //+1 for the comma that is missing here
+            }
+
+            if i == centre - 1 {
+                result.push_str("    ");
+            }
+        }
+        result.push_str("\n");
     }
     result
 }
@@ -231,9 +262,13 @@ mod tests {
        KC_Q, KC_W,
         "#;
 
-        let pairs = MyParser::parse(Rule::layer, example).unwrap();
+        let pairs = MyParser::parse(Rule::layer, example)
+            .unwrap()
+            .nth(0)
+            .unwrap()
+            .into_inner();
         println!("{:?}", pairs);
-        assert_eq!(pairs.count(), 3); //includes EOI
+        assert_eq!(pairs.count(), 2);
     }
 
     #[test]
@@ -245,9 +280,13 @@ mod tests {
             CW_TOGG ,KC_W
         "#;
 
-        let pairs = MyParser::parse(Rule::layer, example).unwrap();
+        let pairs = MyParser::parse(Rule::layer, example)
+            .unwrap()
+            .next()
+            .unwrap()
+            .into_inner();
         println!("{pairs:?}");
-        assert_eq!(pairs.count(), 4 + 1); //includes EOI
+        assert_eq!(pairs.count(), 4);
     }
 
     #[test]
