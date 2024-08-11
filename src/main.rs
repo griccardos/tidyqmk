c5e75c182ba7b392f45c894c9c7becea5b429cb7
Riccardo <riccardo@binaryarm.com>
2024-08-11 19:54:13 +0200

    change default keymap

--- src/main.rs
+++ src/main.rs
@@ -17,20 +17,20 @@ fn main() {
     let mut ops = PrintOptions::default();
     ops.draw_nice = true;
     let example = r#"
-        [0] = LAYOUT_universal(
-          KC_Q, KC_W, KC_F, KC_P, KC_B,                                             KC_1, KC_2, KC_U,    KC_Y,   KC_SCLN,
-          LSFT_T(KC_A), KC_R, KC_S, KC_T, KC_G,             KC_M, KC_N, KC_E, KC_I,  KC_O,
-          KC_Z, KC_X, KC_C, KC_D, KC_V,                                             KC_K, KC_H, KC_COMM, KC_DOT, KC_QUOTE,
-          CW_TOGG , QK_REP , KC_DEL, KC_TAB, LT(1,KC_SPC), KC_ESC,   KC_ENT , KC_BSPC,KC_NO,KC_NO,KC_NO,   SCRL_TO
-        ),
+[0]=LAYOUT_split_3x6_3(
+KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSPC,
+KC_LCTL,KC_A,KC_S,KC_D,KC_F,KC_G,KC_H,KC_J,KC_K,KC_L,KC_SCLN,KC_QUOT,
+KC_LSFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_N,KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_ESC,
+KC_LGUI,TL_LOWR,KC_SPC,KC_ENT,TL_UPPR,KC_RALT
 
-        [1] = LAYOUT_universal(
-          KC_Q, KC_W, KC_E, KC_R, KC_T,                                             KC_1, KC_2, KC_U,    KC_Y,   KC_SCLN,
-          KC_A, KC_R, KC_S, KC_T, KC_G,             KC_M, KC_N, KC_E, KC_I,  KC_O,
-          KC_Z, KC_X, KC_C, KC_D, KC_V,                                             KC_K, KC_H, KC_COMM, KC_DOT, KC_QUOTE,
-          CW_TOGG , QK_REP , KC_DEL, KC_TAB,KC_SPC, KC_ESC,   KC_ENT , KC_BSPC,KC_NO,KC_NO,KC_NO,   SCRL_TO
-        )
-
+),
+      
+[1]=LAYOUT_split_3x6_3(
+KC_TAB,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_BSPC,
+KC_LCTL,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,XXXXXXX,XXXXXXX,
+KC_LSFT,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+KC_LGUI,_______,KC_SPC,KC_ENT,_______,KC_RALT
+),
         "#;
 
     let prog = match MyParser::parse(Rule::programouter, example) {
